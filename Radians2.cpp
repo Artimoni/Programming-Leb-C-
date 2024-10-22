4943f3df1b7195da7526cc981c5dbf686af5b99e
Artimoni <76207858+Artimoni@users.noreply.github.com>
2024-10-22 14:08:08 +0800

    Add files via upload

---
+++ Radians2.cpp
@@ -0,0 +1,36 @@
+﻿#include <cmath>
+#include <iostream> //Для числа Пи M_PI
+
+
+using namespace std;
+void main()
+{
+    const double Pi = 3.141592653589793;
+    setlocale(LC_ALL, "Rus"); //Добавление символов русского значения
+    double radian;
+    cout << "Введите радианную меру:\n";
+    cin >> radian; //Ввод радианной меры
+    double degreesZ, degreesZ1;
+    degreesZ1 = radian * (180 / Pi);
+    degreesZ = radian * (180 / Pi) - floor(radian * (180 / Pi));
+    double minute;
+    minute = degreesZ * 60;
+    double minuteZ;
+    minuteZ = minute - floor(minute);
+    double sec;
+    sec = minuteZ * 60;
+
+
+    cout << "Ответ:" << endl;
+    cout << "Градусы: " << degreesZ1 << "\nМинуты: " << minute << "\nСекунды: " << sec << endl;
+}
+
+
+
+
+
+
+
+
+
+
