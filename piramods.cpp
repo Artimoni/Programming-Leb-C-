70c10519c981ae2c9d610d952a86bf96bb570ba6
Artimoni <76207858+Artimoni@users.noreply.github.com>
2024-10-22 14:10:47 +0800

    Add files via upload

---
+++ piramods.cpp
@@ -0,0 +1,14 @@
+﻿#include <cmatch>
+#include <iostream>;
+using namespace std;
+void main()
+{
+	const double Pi = 3.141592653589793;
+	setlocale(LC_ALL, "Rus");    //Добавляем русские символы в консоль
+	int r, a;
+	cout << "Введите радиус окружности и сторону правильного треугольника: ";
+	cin >> a >> r;
+	double Sr = Pi * pow(r, 2);
+	double Sa = (pow(a, 2) * sqrt(3)) / 4;
+	cout << "Количество кругов: " << floor(Sa / Sr) << endl;
+}
