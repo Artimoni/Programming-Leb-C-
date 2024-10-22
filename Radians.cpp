daddfce272bd363f98505ea48952dd2cb8003dfe
Artimoni <76207858+Artimoni@users.noreply.github.com>
2024-10-22 14:07:22 +0800

    Add files via upload

---
+++ Radians.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <cmath>; // для числа Пи M_PI
+
+using namespace std;
+void main()
+{
+	const double Pi = 3.141592653589793; // Значение ПИ
+
+	setlocale(LC_ALL, "Rus");    //Добавление символов русского значения
+	double degress, min, sec;
+	cout << "Введите градусы, минуты, секунды:\n";
+	cin >> degress >> min >> sec; // Ввод данных
+	double radian;
+	radian = (degress + (min + sec / 60) / 60 * fabs(degress) / degress) * Pi / 180;// Данные переводятся в радианы
+	cout << radian << endl;
+}
